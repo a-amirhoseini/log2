a942e7f938ad8cb95710571a15bb5150b9190198
a-amirhoseini <90419864+a-amirhoseini@users.noreply.github.com>
2021-09-11 18:09:07 +0430

    Update log10.py

--- log10.py
+++ log10.py
@@ -1,14 +1,3 @@
-def log2(nu):
-    num = int(nu)
-    num_log2 = ''
-    while num >= 1 :
-        num_log2 = str(num % 2) + num_log2
-        num = num // 2
-    return " عدد شما در مبنای 2 : " + num_log2 + " است. "
-# مثال:
-number = input('لطفا عدد خود را در مبنای 10 وارد کنید تا ما آن را در مبنای 2  تحویل دهیم:  ')
-print(log2(number))
-
 def log10(nu):
     num = nu[::-1]
     num_log10 = 0
