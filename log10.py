977454e5f995ff8bab1e893e0bc292e40d2d00d2
a-amirhoseini <a.amirhoseini@gmail.com>
2021-09-15 18:18:36 +0430

    improve

--- log10.py
+++ log10.py
@@ -7,7 +7,3 @@ def log10(nu):
             num_log10 += 2 ** dor
         dor += 1
     return ' عدد شما در مبنای 10 ' + str(num_log10) + ' می باشد. '
-
-# مثال:
-number = input('لطفا عدد خود را در مبنای 2 وارد نمایید تا آن را در مبنای 10 تحویل دهیم:  ')
-print(log10(number))
