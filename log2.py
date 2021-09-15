affd2a50ac74815aec0f3d6f8ee1f950df855320
a-amirhoseini <a.amirhoseini@gmail.com>
2021-09-15 18:17:56 +0430

    improve

--- log2.py
+++ log2.py
@@ -5,6 +5,3 @@ def log2(nu):
         num_log2 = str(num % 2) + num_log2
         num = num // 2
     return " عدد شما در مبنای 2 : " + num_log2 + " است. "
-# مثال:
-number = input('لطفا عدد خود را در مبنای 10 وارد کنید تا ما آن را در مبنای 2  تحویل دهیم:  ')
-print(log2(number))
