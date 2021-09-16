5730f7cb959180ce7177df9ad0f7f051fc18eb98
a-amirhoseini <a.amirhoseini@gmail.com>
2021-09-16 13:44:13 +0430

    testing

--- log2.py
+++
@@ -1,7 +0,0 @@
-def log2(nu):
-    num = int(nu)
-    num_log2 = ''
-    while num >= 1 :
-        num_log2 = str(num % 2) + num_log2
-        num = num // 2
-    return " عدد شما در مبنای 2 : " + num_log2 + " است. "
