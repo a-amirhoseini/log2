7eaf747721793fbb11b4b89dccc192333eb183d4
a-amirhoseini <a.amirhoseini@gmail.com>
2021-09-16 13:44:55 +0430

    testing

--- log10.py
+++
@@ -1,9 +0,0 @@
-def log10(nu):
-    num = nu[::-1]
-    num_log10 = 0
-    dor = 0
-    for n in num:
-        if not int(n) == 0 :
-            num_log10 += 2 ** dor
-        dor += 1
-    return ' عدد شما در مبنای 10 ' + str(num_log10) + ' می باشد. '
